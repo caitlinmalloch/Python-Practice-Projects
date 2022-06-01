991b63936d7d0ea20e2f3ad3536f436e273afbb6
Caitlin Malloch <96154629+caitlinmalloch@users.noreply.github.com>
2022-05-31 17:12:53 -0700

    file that draws initials with ASCII art

---
+++ initials.py
@@ -0,0 +1,12 @@
+#Caitlin Malloch
+#Fun fact: I am learning how to code in Python.
+
+name = """
+ CCC     M     M
+C   C    MM   MM
+C        MM   MM
+C        M  M  M
+C   C    M     M
+ CCC     M     M
+"""
+print(name)
